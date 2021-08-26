6cb6a18ea25683393afc76173e4b2bd7efb44c19
Jakub Boniecki <extripl@gmail.com>
2021-08-27 01:47:09 +0200

    Add network serialization for IPlaceable

---
+++ Assets/Scripts/Core/Serialization/IPlaceableSerialization.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+
+namespace Minecraft.Core.Serialization
+{
+    public static class IPlaceableSerialization
+    {
+        public static void WriteIPlaceableObject(this NetworkWriter writer, IPlaceable placeable)
+        {
+            writer.WriteString(placeable.RegistryName);
+        }
+
+        public static IPlaceable ReadIPlaceableObject(this NetworkReader reader)
+        {
+            IPlaceable placeable = (IPlaceable)RegistryManager.GetRegistryObject(reader.ReadString());
+
+            return placeable;
+        }
+    }
+}
