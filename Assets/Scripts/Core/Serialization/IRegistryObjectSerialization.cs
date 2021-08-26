5aab1e4e497830fc39bf8278e5b3c5779fbbb3c9
Jakub Boniecki <extripl@gmail.com>
2021-08-26 15:02:44 +0200

    Add Mirror serialization methods for IRegistryObject

---
+++ Assets/Scripts/Core/Serialization/IRegistryObjectSerialization.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+
+namespace Minecraft.Core.Serialization
+{
+    public static class IRegistryObjectSerialization
+    {
+        public static void WriteIRegistryObject(this NetworkWriter writer, IRegistryObject registryObject)
+        {
+            writer.WriteString(registryObject.RegistryName);
+        }
+
+        public static IRegistryObject ReadIRegistryObject(this NetworkReader reader)
+        {
+            IRegistryObject registryObject = RegistryManager.GetRegistryObject(reader.ReadString());
+
+            return registryObject;
+        }
+    }
+}
